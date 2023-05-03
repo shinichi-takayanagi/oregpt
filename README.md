2d713a7715eccf2cc1a98bd35e698e5914e21651
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2023-05-03 09:32:33 +0900

    Update

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
 # oregpt
-GPT CLI tool for me
+GPT CLI tool for me.
+It's a very minimal cli prompt, where you can chat and save the conversation as json.
 
+![workflow](https://github.com/shinichi-takayanagi/oregpt/actions/workflows/main.yml/badge.svg)
 
-#
-```
-pip install oregpt
-```
-
-```
-cat config.yml
-```
-
-
-```
-vi ~/.zshrc
-alias gpt3='python /PATH/TO/PROG/gpt3.py'
+## WIP
+```bash
+poetry run python -m oregpt.main
 ```
--- oregpt/main.py
+++ oregpt/main.py
@@ -3,12 +3,8 @@ import os
 import openai
 import yaml
 
+from typing import Any
 from oregpt.chat_bot import ChatBot
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-with open("config.yml", "r") as yamlfile:
-    data = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
 blue = "\033[34m"
 green = "\033[32m"
@@ -16,14 +12,37 @@ bold = "\033[1m"
 reset = "\033[0m"
 
 
+def initialize_open_ai_key(data: dict[str, Any]) -> None:
+    if "api_key" in data["openai"]:
+        openai.api_key = data["openai"]["api_key"]
+        return
+    if value := os.getenv("OPENAI_API_KEY"):
+        openai.api_key = value
+        return
+
+    raise LookupError("OpenAI's API key was not found in config.yml and environment variables")
+
+
 def main() -> None:
-    bot = ChatBot(data["model"])
+    with open("config.yml", "r") as yamlfile:
+        data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    initialize_open_ai_key(data)
+    bot = ChatBot(data["openai"]["model"])
     while True:
-        print("")
-        prompt = input(bold + blue + "Enter a prompt: " + reset)
-        if prompt.lower() in ["q", "exit", "quit"]:
-            break
-        bot.respond(prompt)
+        print("\n")
+        prompt = input(bold + blue + "Me: " + reset).lower()
+        print("\n")
+        match prompt:
+            case "exit" | "quit" | "q":
+                break
+            case "clear" | "reboot":
+                bot.clear()
+                print(f"Clear all conversation history")
+            case "save":
+                file_name = bot.save(data["log"])
+                print(f"Save all conversation history in {file_name}")
+            case _:
+                bot.respond(prompt)
 
 
 if __name__ == "__main__":
