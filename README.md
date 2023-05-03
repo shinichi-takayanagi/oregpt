db666918e9ff92f65ed98ba1f93f51059b5968b3
Shinichi Takaŷanagi <shinichi.takayanagi@gmail.com>
2023-05-03 09:46:36 +0900

    Initial Development (#2)
    Initial dev

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # oregpt
-GPT CLI tool for me
+GPT CLI tool for me.
+It's a very minimal cli prompt, where you can chat and save the conversation as json.
+
+![workflow](https://github.com/shinichi-takayanagi/oregpt/actions/workflows/main.yml/badge.svg)
+
+## WIP
+```bash
+poetry run python -m oregpt.main
+```
---
+++ oregpt/main.py
@@ -0,0 +1,49 @@
+import os
+from typing import Any
+
+import openai
+import yaml
+
+from oregpt.chat_bot import ChatBot
+
+blue = "\033[34m"
+green = "\033[32m"
+bold = "\033[1m"
+reset = "\033[0m"
+
+
+def initialize_open_ai_key(data: dict[str, Any]) -> None:
+    if "api_key" in data["openai"]:
+        openai.api_key = data["openai"]["api_key"]
+        return
+    if value := os.getenv("OPENAI_API_KEY"):
+        openai.api_key = value
+        return
+
+    raise LookupError("OpenAI's API key was not found in config.yml and environment variables")
+
+
+def main() -> None:
+    with open("config.yml", "r") as yamlfile:
+        data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    initialize_open_ai_key(data)
+    bot = ChatBot(data["openai"]["model"])
+    while True:
+        print("\n")
+        prompt = input(bold + blue + "Me: " + reset).lower()
+        print("\n")
+        match prompt:
+            case "exit" | "quit" | "q":
+                break
+            case "clear" | "reboot":
+                bot.clear()
+                print(f"Clear all conversation history")
+            case "save":
+                file_name = bot.save(data["log"])
+                print(f"Save all conversation history in {file_name}")
+            case _:
+                bot.respond(prompt)
+
+
+if __name__ == "__main__":
+    main()
